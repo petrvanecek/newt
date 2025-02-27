2977960a02c50e9e5fc368705ec66259c44c1901
Petr Vanecek <pvanecek@gapps.zcu.cz>
2025-02-27 19:22:41 +0100

    .

--- bot.js
+++ bot.js
@@ -26,11 +26,11 @@ let newtCommand = new SlashCommandBuilder()
     .setName('newt')
     .setDescription('Interacts with the planetary system');
 
-    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    /*const guild = client.guilds.cache.get(process.env.GUILD_ID);
     await guild.commands.set([]); // Smaž všechny příkazy
     console.log("🗑️ Příkazy odstraněny!");
     
-    await new Promise(resolve => setTimeout(resolve, 5000)); // Počkej 5 sekund, aby Discord stihl provést změny
+    await new Promise(resolve => setTimeout(resolve, 5000)); // Počkej 5 sekund, aby Discord stihl provést změny*/
     
 
 for (const file of commandFiles) {
